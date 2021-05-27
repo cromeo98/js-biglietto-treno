33eb8f9660dff56cf9c65049953702e339e5fc58
cromeo98 <romeo.christian98@gmail.com>
2021-05-27 16:26:48 +0200

    end: solution 1 + bonus

--- js/script.js
+++ js/script.js
@@ -2,19 +2,35 @@
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole: il prezzo del biglietto è definito in base ai km (0.21 € al km) va applicato uno sconto del 20% per i minorenni va applicato uno sconto del 40% per gli over 65. FATTO
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
+alert('Ciao! rispondi alle seguenti domande per sapere il prezzo del tuo biglietto! Buon viaggio!')
+
 var km = prompt('Quanti km vuoi percorrere?');
 // console.log(km);
 var age = prompt('Quanti anni hai?');
 // console.log(age);
 
 var pricePerKm = km * 0.21;
-// console.log(pricePerKm);
+
+console.log(pricePerKm);
 
 if (age < 18){
     pricePerKm = pricePerKm - pricePerKm * 20 / 100;
+    document.getElementById("row-young-discount").style.backgroundColor = "yellow";
 } else if (age >= 65){
     pricePerKm = pricePerKm - pricePerKm * 40 / 100;
+    document.getElementById("row-old-discount").style.backgroundColor = "yellow";
+} else{
+    document.getElementById("row-no-discount").style.backgroundColor = "yellow";
 }
+
 console.log(pricePerKm);
+
+document.getElementById('price-per-km').innerHTML = 'Il prezzo del tuo biglietto è: ' + pricePerKm.toFixed(2) + ' €';
+
+document.getElementById('km-scelti').innerHTML = 'Hai scelto di percorrere: ' + km + ' km';
+
+document.getElementById('eta').innerHTML = 'Hai ' + age + ' anni';
+
+document.getElementById('text').innerHTML = 'Grazie per averci comunicato i tuoi dati, qui sotto puoi verificare i dati inseriti e consultare il prezzo del biglietto.';
 
 console.log('ciao');
